661c155fffe63d780cbec84ecf0606c23c7a22af
Hansa Gennoo <hansagennoo10@gmail.com>
2025-10-07 21:49:53 +0400

    Slug added to landing_page

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2025_10_07_145005) do
+ActiveRecord::Schema[7.1].define(version: 2025_10_07_174738) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -21,6 +21,8 @@ ActiveRecord::Schema[7.1].define(version: 2025_10_07_145005) do
     t.string "theme"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "slug"
+    t.index ["slug"], name: "index_landing_pages_on_slug", unique: true
     t.index ["user_id"], name: "index_landing_pages_on_user_id"
     t.index ["username"], name: "index_landing_pages_on_username", unique: true
   end
