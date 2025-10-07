1d562a2f72acc371d72a697a98bce9e1700c4de1
Hansa Gennoo <hansagennoo10@gmail.com>
2025-10-07 23:49:26 +0400

    Created the themes controller and added the route path as well as the form view.

---
+++ app/controllers/themes_controller.rb
@@ -0,0 +1,43 @@
+class ThemesController < ApplicationController
+  def show
+    @theme = Theme.find(params[:id])
+  end
+
+  def new
+    @theme = Theme.new
+  end
+
+  def create
+    @theme = Theme.new
+    if @theme.save
+      redirect_to themes_path
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  def edit
+    @theme = Theme.find(params[:id])
+  end
+
+  def update
+    @theme = Theme.find(params[:id])
+    @theme.update(theme_params)
+
+    redirect_to themes_path
+  end
+
+  def destroy
+    @theme = Them.find(params[:id])
+    @theme.destroy
+
+    redirect_to themes_path, status: :see_other
+  end
+
+  private
+
+  def theme_params
+    require.params(:theme).permit(:name, :key, :base, :primary_colour, :accent_colour, :background, :font_heading, :font_body, :customisable)
+
+  end
+end
