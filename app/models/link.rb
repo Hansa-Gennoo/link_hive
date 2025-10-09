8529124b0196dcdb38242b896b1e586bb308e92c
Hansa Gennoo <hansagennoo10@gmail.com>
2025-10-09 23:32:22 +0400

    Changes made to the models, controllers and views for better navigation and improved UX

--- app/models/link.rb
+++ app/models/link.rb
@@ -1,3 +1,16 @@
 class Link < ApplicationRecord
   belongs_to :landing_page
+
+  validates :title, :url, presence: true
+
+  before_create :set_default_position
+
+  private
+
+  def set_default_position
+    if position.nil?
+      max_position = landing_page.links.maximum(:position) || 0
+      self.position = max_position + 1
+    end
+  end
 end
